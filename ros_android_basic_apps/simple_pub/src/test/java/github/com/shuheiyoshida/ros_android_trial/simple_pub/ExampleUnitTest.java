39b0095cbd20aac70647097b4847850b5b593ede
Shuhei_YOSHIDA <s-yoshida@frontier.hokudai.ac.jp>
2020-08-05 15:24:42 +0900

    add module, simple_pub

---
+++ ros_android_basic_apps/simple_pub/src/test/java/github/com/shuheiyoshida/ros_android_trial/simple_pub/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package github.com.shuheiyoshida.ros_android_trial.simple_pub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
